5bddf5e780927164f153e90197eefb20bd85f63d
shanjana <shanjana@DESKTOP-CFTVV8M>
2017-10-24 13:36:47 +0200

    k

--- WcfServ/IService1.cs
+++ WcfServ/IService1.cs
@@ -13,6 +13,7 @@ namespace WcfServ
     public interface IService1
     {
 
+
         [OperationContract]
         string GetData(int value);
 
--- WcfServ/Service1.svc.cs
+++ WcfServ/Service1.svc.cs
@@ -14,6 +14,8 @@ namespace WcfServ
     {
         public string GetData(int value)
         {
+
+
             return string.Format("You entered: {0}", value);
         }
 
